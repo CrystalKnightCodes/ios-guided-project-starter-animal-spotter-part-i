7d8de6dca819af7ee83853e08637dc1031460940
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-11-06 10:33:29 -0500

    Configured Log In

--- AnimalSpotter/Application/APIController.swift
+++ AnimalSpotter/Application/APIController.swift
@@ -17,6 +17,7 @@ enum HTTPMethod: String {
 class APIController {
     
     private let baseUrl = URL(string: "https://lambdaanimalspotter.vapor.cloud/api")!
+    var bearer: Bearer?
     
     // create function for sign up
     func signUp(with user: User, completion: @escaping (Error?) -> ()) {
@@ -47,14 +48,57 @@ class APIController {
                 return
             }
             
-        }
+            completion(nil)
+        }.resume()
     }
     
     // create function for sign in
     func signIn(with user: User, completion: @escaping (Error?) -> ()) {
         
+        let loginURL = baseUrl.appendingPathComponent("users/login")
+        var request = URLRequest(url: loginURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding user object \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            if let error = error {
+                 completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                completion(NSError())
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            do {
+                self.bearer = try decoder.decode(Bearer.self, from: data)
+            } catch {
+                print("Error decoding bearer object: \(error)")
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+        }.resume()
     }
-    
     // create function for fetching all animal names
     
     // create function for fetching a specific animal
---
+++ AnimalSpotter/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  AnimalSpotter
+//
+//  Created by Christy Hicks on 11/6/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+}
