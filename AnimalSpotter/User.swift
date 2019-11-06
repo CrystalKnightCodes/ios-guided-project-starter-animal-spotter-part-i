f79a27dfda7c828334b138ee76d359966d6b985b
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-11-06 08:46:08 -0500

    Initial Setup

---
+++ AnimalSpotter/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  AnimalSpotter
+//
+//  Created by Christy Hicks on 11/5/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    let username: String
+    let password: String
+}
